970b3e7724032123f145294a1d07f753de4d7191
AKSHAY kALRA <102737696+akay72@users.noreply.github.com>
2023-08-10 12:31:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,8 @@ from selenium.webdriver.common.by import By
 from twocaptcha import TwoCaptcha
 import requests
 from selenium.webdriver.chrome.options import Options
-import traceback
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.firefox.options import Options
+import os  # Imported the os library
+
 # Set the layout to 'wide'
 st.set_page_config(layout="wide")
 
@@ -24,46 +18,35 @@ def solve_captcha(captcha_image_path):
     return result
 
 def process_url(url):
-    try:
-        options = Options()
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        
-        # Specify chromedriver path here
-        
-        
-        options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
-        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    time.sleep(10)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    form = soup.find('form')
+    captcha_image = form.find('img')['src']
 
-        
-        
-        driver.get(url)
-        time.sleep(10)
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        form = soup.find('form')
-        captcha_image = form.find('img')['src']
+    response = requests.get(captcha_image)
+    captcha_image_path = 'captcha_image.jpg'
+    with open(captcha_image_path, 'wb') as fp:
+        fp.write(response.content)
 
-        response = requests.get(captcha_image)
-        captcha_image_path = 'captcha_image.jpg'
-        with open(captcha_image_path, 'wb') as fp:
-            fp.write(response.content)
+    captcha_solution = solve_captcha(captcha_image_path)
 
-        captcha_solution = solve_captcha(captcha_image_path)
-        captcha_input_field = driver.find_element(By.XPATH,'//*[@id="captchacharacters"]')
-        captcha_input_field.send_keys(captcha_solution['code'])
-        time.sleep(5)
-        submit_button = driver.find_element(By.XPATH, "//button[@type='submit' and @class='a-button-text' and contains(text(), 'Continue shopping')]")
-        submit_button.click()
-        time.sleep(10)
-        page_html = driver.page_source
-        driver.quit()
-        return page_html
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        st.text(traceback.format_exc())
-        return None
+    # Delete the CAPTCHA image
+    os.remove(captcha_image_path)
+
+    captcha_input_field = driver.find_element(By.XPATH,'//*[@id="captchacharacters"]')
+    captcha_input_field.send_keys(captcha_solution['code'])
+    time.sleep(5)
+    submit_button = driver.find_element(By.XPATH, "//button[@type='submit' and @class='a-button-text' and contains(text(), 'Continue shopping')]")
+    submit_button.click()
+    time.sleep(10)
+    page_html = driver.page_source
+    driver.quit()
+    return page_html
 
 def main():
     st.title('CAPTCHA Solver for Amazon')
@@ -88,7 +71,7 @@ def main():
         """)
 
     elif page_selection == "CAPTCHA Solver":
-        
+        st.image("https://www.streamlit.io/images/brand/streamlit-mark-color.svg", width=150)
         st.write("""
         Enter the URL of the Amazon product page, and we'll handle the CAPTCHA for you!
         """)
@@ -99,8 +82,9 @@ def main():
                 return
 
             with st.spinner('Processing...'):
-                page_html = process_url(url)
-                if page_html:
+                try:
+                    page_html = process_url(url)
+
                     st.download_button(
                         label="Download Page HTML",
                         data=page_html.encode('utf-8'),
@@ -108,6 +92,8 @@ def main():
                         mime='text/plain'
                     )
                     st.success('CAPTCHA Processed! Download the page content using the button above.')
+                except:
+                    st.error('Sorry, something went wrong. Please try again.')
 
 if __name__ == '__main__':
     main()
