079f55f2407c6713c2f9b1269d55c41c304c1f32
AKSHAY kALRA <102737696+akay72@users.noreply.github.com>
2023-08-10 14:27:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def process_url(url):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        
+        options.add_argument("--log-path=chromedriver.log")
+
         driver = webdriver.Chrome(options=options)
         
         driver.get(url)
